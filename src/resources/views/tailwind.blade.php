fa16d3a02f815f7e6e91a6baceae0706357d4afc
AliReaza <alireazamagh@gmail.com>
2023-01-19 21:09:57 +0330

    Install and config IDE Helper and Tailwind with Vite

---
+++ src/resources/views/tailwind.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Plivios</title>
+
+    @vite(['resources/css/app.css', 'resources/css/tailwind.scss'])
+</head>
+<body>
+<h1 class="text-3xl font-bold">
+    Hello world! Welcome to Plivios Prototype
+</h1>
+</body>
+</html>
